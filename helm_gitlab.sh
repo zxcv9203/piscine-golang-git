d31812409642606aa524792b0f55d0d4f4bcea40
yongckim <zxcv9203@naver.com>
2021-12-29 22:42:04 +0900

    gitlab helm chart values modify

--- helm_gitlab.sh
+++ helm_gitlab.sh
@@ -5,11 +5,12 @@ helm repo add gitlab https://charts.gitlab.io/
 # DOMAIN_NAME = gitlab에 접근하는 도메인 네임
 # EMAIL = Let's Encrypt에서 인증서를 발급할 수 있는 이메일 주소
 # KUBERNETES_NAMESPACE = 
-helm install gitlab gitlab/gitlab \
---set global.hosts.domain=$DOMAIN_NAME \
---set certmanager-issuer.email=$EMAIL \
---namespace $KUBERNETES_NAMESPACE
+#helm install gitlab gitlab/gitlab \
+#--set global.hosts.domain=$DOMAIN_NAME \
+#--set certmanager-issuer.email=$EMAIL \
+#--namespace $KUBERNETES_NAMESPACE
 
+helm install -n yongckim -f piscine_values.yaml gitlab gitlab/gitlab
 
 #helm 삭제를 하고 싶을 경우 다음 명령 입력
 # helm uninstall gitlab
